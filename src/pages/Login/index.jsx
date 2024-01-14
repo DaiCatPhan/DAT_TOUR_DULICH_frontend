ccac786ee43720791cc8d886ae10ace07d98fb40
PHAN DAI CAT <phandaicat12032002@gmail.com>
2024-01-14 14:52:41 +0700

    module login UI

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,7 +3,7 @@ import styles from "./Login.module.scss";
 const cx = className.bind(styles);
 import { useNavigate } from "react-router-dom";
 
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Checkbox, Divider, Form, Input } from "antd";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 
 import AuthService from "../../services/AuthService";
@@ -13,6 +13,8 @@ import { toast } from "react-toastify";
 
 import { useDispatch, useSelector } from "react-redux";
 import { doLoginAction } from "../../redux/account/accountSlide";
+
+import { IconChevronsLeft } from "@tabler/icons-react";
 
 function LoginPage() {
   const dispatch = useDispatch();
@@ -38,78 +40,115 @@ function LoginPage() {
   };
 
   return (
-    <div className={cx("wrapper")}>
-      <div className={cx("row border m-auto", "framLogin")}>
-        <div className={cx("col-lg")}>
-          <div className={cx("bgLogin", "w-100 h-100")}></div>
-        </div>
-        <div className={cx("col-lg-8  d-flex align-items-center")}>
-          <div
-            className={cx("border")}
-            style={{ minWidth: 500, margin: "0 auto" }}
-          >
-            <h2 className={cx("py-3 text-center")}>Đăng nhập</h2>
+    <div className={cx("wrapper  ", "bg-light ")}>
+      <div
+        className={cx(
+          "d-flex justify-content-center align-items-center vh-100  "
+        )}
+      >
+        <div className={cx("row border bg-white", "frameLogin")}>
+          <div className={cx("col-lg-4   p-0 ")}>
+            <div className={cx("bgLogin")}></div>
+          </div>
 
-            <Form
-              name="normal_login"
-              className={cx("login-form")}
-              onFinish={onFinish}
+          <div className={cx("col-lg-8 position-relative  p-0")}>
+            <div className={cx("position-absolute", "iconBack")}>
+              <Link to={"/"}>
+                <IconChevronsLeft />
+              </Link>
+            </div>
+            <div
+              className={cx(
+                "d-flex justify-content-center align-items-center h-100  "
+              )}
             >
-              <Form.Item
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Vui lòng nhập Email!",
-                  },
-                ]}
-              >
-                <Input
-                  prefix={
-                    <UserOutlined className={cx("site-form-item-icon")} />
-                  }
-                  placeholder="Email"
-                />
-              </Form.Item>
+              <div className={cx(" ")}>
+                {/* <div>
+                  <h4>Đăng nhập</h4>
+                  <div>
+                    <button>Sing in with google</button>
+                  </div>
+                </div> */}
 
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Vui lòng nhập mật khẩu !",
-                  },
-                ]}
-              >
-                <Input
-                  prefix={
-                    <LockOutlined className={cx("site-form-item-icon")} />
-                  }
-                  type="password"
-                  placeholder="Mật khẩu"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>Ghi nhớ tôi</Checkbox>
-                </Form.Item>
-
-                <a className={cx("login-form-forgot")} href="">
-                  Quên mật khẩu
-                </a>
-              </Form.Item>
-
-              <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className={cx("login-form-button")}
+                <Form
+                  name="normal_login"
+                  className={cx("login-form")}
+                  onFinish={onFinish}
+                  labelCol={{
+                    span: 8,
+                  }}
+                  wrapperCol={{
+                    span: 24,
+                  }}
+                  style={{
+                    maxWidth: 1000,
+                  }}
                 >
-                  Đăng nhập
-                </Button>
-                Hoặc <Link to={"/register"}>Đăng ký!</Link>
-              </Form.Item>
-            </Form>
+                  <Divider>
+                    <p>Hoặc đăng nhập với email</p>
+                  </Divider>
+
+                  <Form.Item
+                    name="email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Vui lòng nhập Email!",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={
+                        <UserOutlined className={cx("site-form-item-icon")} />
+                      }
+                      placeholder="Email"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Vui lòng nhập mật khẩu !",
+                      },
+                    ]}
+                  >
+                    <Input
+                      prefix={
+                        <LockOutlined className={cx("site-form-item-icon")} />
+                      }
+                      type="password"
+                      placeholder="Mật khẩu"
+                    />
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                      <Checkbox>Ghi nhớ tôi</Checkbox>
+                    </Form.Item>
+
+                    <a className={cx("login-form-forgot")} href="">
+                      Quên mật khẩu
+                    </a>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      className={cx("login-form-button")}
+                    >
+                      Đăng nhập
+                    </Button>
+                    <div className={cx("text-center mt-3")}>
+                      Bạn chưa có tài khoản ?{" "}
+                      <Link to={"/register"}>Đăng ký!</Link>
+                    </div>
+                  </Form.Item>
+                </Form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -118,3 +157,6 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
+{
+}
