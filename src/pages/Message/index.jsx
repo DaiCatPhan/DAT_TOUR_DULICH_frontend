8bba3e03868bc53b947f13073139721e394fdf7a
Phan Dai Cat <phandaicat12032002@gmail.com>
2024-03-24 21:26:55 +0700

    module chat message

--- src/pages/Message/index.jsx
+++ src/pages/Message/index.jsx
@@ -2,26 +2,53 @@ import className from "classnames/bind";
 import styles from "./Message.module.scss";
 const cx = className.bind(styles);
 import { io } from "socket.io-client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3000", {
+  transports: ["websocket"],
+});
 
 function Message() {
-  useEffect(() => {
-    const socket = io("http://localhost:3000", { transports: ["websocket"] });
-    socket.on("connection", () => {
-      console.log("Connect to socket.io");
-    });
-    socket.on("new_user_login", (data) => {
-      console.log("new_user_login", data.message);
-    });
-  }, []);
+  //Room State
+  const [room, setRoom] = useState("");
 
-  const handleSend = () => {
-    const socket = io("http://localhost:3000", { transports: ["websocket"] });
-    socket.emit("new_user_login ", { message: "Phan dai cat" });
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
   };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
   return (
-    <div className={cx("wrapper")}>
-      <button onClick={handleSend}>Send message</button>
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {messageReceived}
     </div>
   );
 }
