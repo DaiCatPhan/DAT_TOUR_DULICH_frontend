bfc194167ae2833f945193627d0b9e76cd2f77ef
Phan Dai Cat <phandaicat12032002@gmail.com>
2024-03-28 15:36:27 +0700

    module message

--- src/pages/Message/index.jsx
+++ src/pages/Message/index.jsx
@@ -4,51 +4,96 @@ const cx = className.bind(styles);
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
 
+import { Button, Input } from "antd";
+const { TextArea } = Input;
+
 const socket = io.connect("http://localhost:3000", {
   transports: ["websocket"],
 });
 
 function Message() {
   //Room State
-  const [room, setRoom] = useState("");
-
-  // Messages States
-  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState(2);
+  const [createRoomSever, setCreateRoomSever] = useState();
+  console.log("createRoomSever >>>.", createRoomSever);
+  const [text, setText] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
 
   const joinRoom = () => {
     if (room !== "") {
-      socket.emit("join_room", room);
+      socket.emit("join_room", { room });
     }
   };
 
+  // const sendMessage = () => {
+  //   socket.emit("send_message", { message, room });
+  // };
+
   const sendMessage = () => {
-    socket.emit("send_message", { message, room });
+    socket.emit("send_message", { text, room: createRoomSever, ID_User: 2 });
   };
 
   useEffect(() => {
+    // Lắng nghe sự kiện "room_created" từ máy chủ
+    socket.on("room_created", (room) => {
+      setCreateRoomSever(room); // Cập nhật số phòng mới được tạo
+    });
+
     socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
+      setMessageReceived(data.text);
     });
   }, [socket]);
   return (
-    <div className="App">
-      <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
+    <div className={cx("mx-5")}>
+      {/* <div>
+        <input
+          placeholder="Room Number..."
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <button onClick={joinRoom}> Join Room</button>
+      </div>
+      <div>
+        <input
+          placeholder="Message..."
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        />
+        <button onClick={sendMessage}> Send Message</button>
+      </div>
+      <div>
+        <div> Message:</div>
+        {messageReceived}
+      </div> */}
+
+      <div className={cx("formMessage")}>
+        {/* <div className={cx("list")}>
+          <div className={cx("chat_message", "received")}>Tin nhắn 1</div>
+          <div className={cx("chat_message", "sent")}>Tin nhắn 2</div>
+          <div className={cx("chat_message", "sent")}>Tin nhắn 2</div>
+          <div className={cx("chat_message", "received")}>Tin nhắn 1</div>
+          <div className={cx("chat_message", "sent")}>Tin nhắn 2</div>
+          <div className={cx("chat_message", "received")}>Tin nhắn 1</div>
+        </div> */}
+
+        <button onClick={joinRoom}>Phòng 1</button>
+
+        <div>{messageReceived}</div>
+
+        <div>
+          <TextArea
+            rows={3}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+          />
+          <Button type="primary" onClick={sendMessage}>
+            Gửi
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
